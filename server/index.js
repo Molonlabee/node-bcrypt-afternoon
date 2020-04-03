b3cc4059eb3ba85df20ad7257dc503f55737a2a7
Molonlabee <janayahernandez@yahoo.com>
2020-04-03 18:04:52 -0400

    server fix

--- server/index.js
+++ server/index.js
@@ -1,40 +1,44 @@
-require(dotenv).config();
-const express = require('express');
-const session = require('express-session');
-const massive = require('massive');
-
-const PORT = 4000;
-const {CONNECTION_STRING, SESSION_SECRET} = process.env;
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const massive = require('massive');  
 const app = express();
+
+// const PORT = 4000;
 
 const authCtrl = require('./controllers/authController');
 const treasureCtrl = require('./controllers/treasureController');
 const auth = require('./middleware/authMiddleware');
 
-
-app.use(express.json());
+const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT} = process.env;
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db);
     console.log('db connected');
 });
+app.use(express.json());
 
-app.use(
-    session({
-        resave: true,
-        saveUninitialized: false,
-        secret: SESSION_SECRET,
+app.use(session({
+        resave: false,
+        saveUninitialized: true,
+        cookie:{
+            maxAge: 1000 * 60 * 60 * 24 * 7
+        },
+        secret: SESSION_SECRET
     })
 );
-
+ 
 //ENDPOINTS
 app.post('/auth/register', authCtrl.register);
 app.post('/auth/login', authCtrl.login);
 app.get('/auth/logout', authCtrl.logout);
 
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', treasureCtrl.getUserTreasure);
 app.get('/api/treasure/user', auth.userOnly,treasureCtrl.getUserTreasure);
 app.post('/api/treasure/user', auth.userOnly, treasureCtrl.addUserTreasure);
 app.get('/api/treasure/all', auth.userOnly, auth.adminsOnly, treasureCtrl.getAllTreasure);
 
-app.listen(PORT, ()=> Console.log('Listening on port ${PORT}'));
+app.listen(SERVER_PORT, () => {
+    console.log(`Listening on port ${SERVER_PORT}`)
+});
